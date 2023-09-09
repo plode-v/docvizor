66cd1af5dfa4bbb4eda97fa1ffd0c386f0e7f99b
Plode <84858705+plode-v@users.noreply.github.com>
2023-09-09 19:36:06 +0700

    update frontend

--- client/src/app/(homepage)/page.tsx
+++ client/src/app/(homepage)/page.tsx
@@ -1,24 +1,22 @@
-import React from 'react'
+'use client'
+import React, { useState } from 'react'
 
-const Homepage = async () => {
+
+const Homepage = () => {
+
+  const arr = ['analysis', 'summarization', 'time']
+
   return (
-    <main className='bg-gradient-to-r from-pink-200 to-violet-200 min-h-screen w-screen border-none m-0 flex justify-center items-center flex-col'>
+    <main className='bg-[#FAFAFA] min-h-screen w-screen border-none m-0 flex justify-center items-center flex-col'>
 
-      <section id='hero-banner' className='h-screen w-screen border-black border-2 flex justify-center items-center'>
-        <div className='h-3/4 border-black border-2 w-full 2xl:w-3/4 grid grid-cols-3'>
-          <div className='col-span-2 border inline-block'>
-            <h1 className='font-bold'>
-              Revolutionize
-            </h1>
-            <h1>
-              Take the document analysis to the next level
-            </h1>
-          </div>
-          <div className='col-span-1 border-black border-2'>
-            blob
-          </div>
+      <section id='hero-banner' className='h-screen w-screen border-black border-2 flex flex-col justify-center items-center'>
+        <div className='flex justify-center items-center flex-col'>
+
+          {/* FIXME: Array of words */}
+          <h1 className='font-[700] text-[3rem] capitalize'>DocVizor</h1>
+          {/* <h1 className='font-[700] text-[3rem]'>Analysis</h1> */}
+          <h1>Time is valuable, let DocVizor help you with your documents.</h1>
         </div>
-
       </section>
 
     </main>
