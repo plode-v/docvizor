cbf6bfe608ea79529f5fa67bafcfd29fe8eb365d
Plode <84858705+plode-v@users.noreply.github.com>
2023-09-09 15:59:32 +0700

    setup frontend

---
+++ client/src/app/(homepage)/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Homepage = async () => {
+  return (
+    <div className='w-screen min-h-screen bg-gradient-to-r from-pink-100 to-violet-200'>
+      <div>
+
+      </div>
+    </div>
+  )
+}
+
+export default Homepage
