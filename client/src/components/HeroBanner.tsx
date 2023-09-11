ac3451174be52802c6f0dd7891b97a5a326ef3ce
Plode-v <plodevanichaka@gmail.com>
2023-09-11 19:51:22 +0700

    update frontend

--- client/src/components/HeroBanner.tsx
+++ client/src/components/HeroBanner.tsx
@@ -11,11 +11,11 @@ const HeroBanner = () => {
       ]
     
   return (
-    <div className='flex flex-col items-center justify-center w-full h-screen'>
-        <h1 className='text-[4.5rem] font-[800] capitalize'>{arr[0]}</h1>
-        <h1 className='capitalize text-[2.15rem] font-[700] leading-tight'>let your documents work for you</h1>
+    <div className='flex flex-col items-center pb-20 justify-center w-full h-screen'>
+        <h1 className='md:text-[4.5rem] text-[2.25rem] font-[800] capitalize'>{arr[0]}</h1>
+        <h1 className='capitalize md:text-[2.15rem] text-[1.1rem] text-center font-[700] leading-tight'>let your documents work for you</h1>
 
-        <button className='mt-20 bg-violet-500 text-[20px] font-[600] text-white px-5 py-3 rounded-full hover:scale-110 duration-100'>Get Started</button>
+        <button className='lg:mt-20 mt-10 bg-violet-500 md:text-[20px] font-[600] text-white px-5 py-3 rounded-full hover:scale-110 duration-100'>Get Started</button>
     </div>
   )
 }
