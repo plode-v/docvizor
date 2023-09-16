abdcbe72e2ae437393d430d6cb480d5e35efc68d
Plode <84858705+plode-v@users.noreply.github.com>
2023-09-16 08:54:54 +0700

    setup file upload

--- client/src/app/dashboard/page.tsx
+++ client/src/app/dashboard/page.tsx
@@ -1,8 +1,38 @@
 import React from 'react'
+import FileUpload from '@/components/FileUpload'
+import SideBar from '@/components/SideBar'
+let pdf: string[] = []
 
 const Dashboard = () => {
   return (
-    <div>Hello dashboard</div>
+    <div className='sm:grid sm:grid-cols-5 h-screen'>
+
+      {/* chat history bar */}
+      <SideBar />
+
+      {/* chat section */}
+      <div className='sm:col-span-4 col-span-full h-full border-black border-2'>
+        <div className='block h-full w-full bg-gradient-to-br from-blue-50 to-blue-200'>
+          <div className='h-[50px] w-full border-2 sm:hidden block'>
+            top section includes:
+              {/* - view uploaded PDF button
+              - burger button to view all chat 
+                - all chat history
+                - settings
+                - upgrade to pro */}
+          </div>
+          {pdf.length > 0 ? (
+            <div>
+
+            </div>
+          ) : (
+            <div className='flex items-center justify-center w-full h-[calc(100%-50px)]'>
+              <FileUpload />
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
   )
 }
 
--- client/src/components/FileUpload.tsx
+++ client/src/components/FileUpload.tsx
@@ -1,10 +1,32 @@
 'use client'
 
 import React from 'react'
+import { useDropzone } from 'react-dropzone'
+import { Inbox } from "lucide-react";
 
 const FileUpload = () => {
+
+  const { getRootProps, getInputProps } = useDropzone({
+    accept: { "application/pdf": [".pdf"] },
+    maxFiles: 1,
+    onDrop: (acceptedFiles) => {
+      console.log(acceptedFiles)
+    }
+  });
+
   return (
-    <div>FileUpload</div>
+    <div className='p-1 sm:p-2 sm:w-1/2 sm:h-1/4 rounded-xl bg-white border-gray-50 border-2'>
+      <div 
+        {...getRootProps()} 
+        className='border-dashed border-2 rounded-xl cursor-pointer flex justify-center items-center h-full bg-gray-50 flex-col p-8'
+      >
+        <input {...getInputProps()} />
+        <>
+        <Inbox className='h-6 sm:h-10 w-6 sm:w-10 text-blue-700' />
+        <p className='capitalize mt-2 sm:text-[14px] text-[10px] text-gray-400 text-center'>Drop your PDF here</p>
+        </>
+      </div>
+    </div>
   )
 }
 
