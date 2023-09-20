38362682d9696173619be7b5ff5551247d4b19f5
Plode <84858705+plode-v@users.noreply.github.com>
2023-09-20 13:31:21 +0700

    setup aws s3

--- src/components/ChatBox.tsx
+++ src/components/ChatBox.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import FileUpload from './FileUpload'
 
 const ChatBox = () => {
   return (
-    <div className='border h-[calc(100vh-90px)]'>ChatBox</div>
+    <div className='border h-[calc(100vh-90px)] flex items-center justify-center'>
+        <FileUpload />
+    </div>
   )
 }
 
--- src/components/FileUpload.tsx
+++ src/components/FileUpload.tsx
@@ -1,8 +1,30 @@
+
+'use client'
+import { Inbox } from 'lucide-react';
 import React from 'react'
+import { useDropzone } from "react-dropzone";
 
 const FileUpload = () => {
+
+    const { getRootProps, getInputProps } = useDropzone({
+        accept: { "application/pdf": [".pdf"] },
+        maxFiles: 1,
+        onDrop: (accpetedFiles) => {
+            console.log(accpetedFiles[0]);
+
+        }
+    })
+
   return (
-    <div>FileUpload</div>
+    <div className='h-1/3 w-1/3 p-2 bg-gray-100 rounded-md'>
+        <div {...getRootProps({
+            className: "border border-dashed h-full border-gray-300 border-2 flex items-center justify-center flex-col cursor-pointer"
+        })}>
+            <input {...getInputProps()} />
+            <Inbox className='h-[30px] w-[30px] text-slate-800' />
+            <p className='capitalize font-[500] text-neutral-400'>drop PDF file here</p>
+        </div>
+    </div>
   )
 }
 
