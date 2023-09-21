6ed98d18e7e13a8ee5850029cf77a22a52107980
Plode <84858705+plode-v@users.noreply.github.com>
2023-09-21 21:22:41 +0700

    setup frontend

---
+++ src/app/(homepage)/page.tsx
@@ -0,0 +1,12 @@
+import HomeSidebar from '@/components/homepage/HomeSidebar'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='max-w-screen min-h-screen'>
+      <HomeSidebar />
+    </div>
+  )
+}
+
+export default page
---
+++ src/components/homepage/HomeSidebar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HomeSidebar = () => {
+  return (
+    <div className='fixed h-screen bg-slate-900 w-[70px] text-white flex justify-center items-between py-5'>
+        Logo
+    </div>
+  )
+}
+
+export default HomeSidebar
